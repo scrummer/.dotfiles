0c3f7e506988289d597f8757e5acd191f584ec63
scrummer <scrummer@gmx.ch>
2022-06-01 22:54:10 +0200

    Add private repo

--- setup.sh
+++ setup.sh
@@ -10,7 +10,8 @@ ln -sf "$DIR_PATH"/.ideavimrc "$HOME"/.ideavimrc
 echo ".vimrc"
 ln -sf "$DIR_PATH"/.vimrc "$HOME"/.vimrc
 echo ".zshrc"
-ln -sf "$DIR_PATH"/.zshrc "$HOME"/.zshrc
+ln -sf "$DIR_PATH"/zsh/.zshrc "$HOME"/.zshrc
 echo ".zsh_profile"
-ln -sf "$DIR_PATH"/.zsh_profile "$HOME"/.zsh_profile
-
+ln -sf "$DIR_PATH"/zsh/.zsh_profile "$HOME"/.zsh_profile
+echo "labymod/zsh/labymod.zsh_profile"
+[[ -f "$DIR_PATH"/labymod/zsh/labymod.zsh_profile ]] && ln -sf "$DIR_PATH/labymod/zsh/labymod.zsh_profile" "$HOME/labymod.zsh_profile"
